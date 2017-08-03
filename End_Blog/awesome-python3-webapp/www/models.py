360d47bda6a143c163e882805bf1c9081d6fa275
834930269 <834930269@qq.com>
2017-08-03 09:24:25 +0800

    End_Blog

--- End_Blog/awesome-python3-webapp/www/models.py
+++ End_Blog/awesome-python3-webapp/www/models.py
@@ -1,31 +1,38 @@
-import time,uuid,logging
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Models for user, blog, comment.
+'''
+
+__author__ = 'Michael Liao'
+
+import time, uuid
 
 from orm import Model, StringField, BooleanField, FloatField, TextField
 
 def next_id():
-    idcard='%015d%s000' %(int(time.time()*1000),uuid.uuid4().hex)
-    logging.info('id is %s ...' % idcard)
-    return idcard
+    return '%015d%s000' % (int(time.time() * 1000), uuid.uuid4().hex)
 
 class User(Model):
-    __table__='users'
+    __table__ = 'users'
 
-    id=StringField(primary_key=True,default=next_id,ddl='varcahr(50)')
-    email=StringField(ddl='varchar(50)')
-    passwd=StringField(ddl='varchar(50)')
-    admin=BooleanField()
-    name=StringField(ddl='varchar(50)')
-    image=StringField(ddl='varchar(500)')
-    created_at=FloatField(default=time.time)
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    email = StringField(ddl='varchar(50)')
+    passwd = StringField(ddl='varchar(50)')
+    admin = BooleanField()
+    name = StringField(ddl='varchar(50)')
+    image = StringField(ddl='varchar(500)')
+    created_at = FloatField(default=time.time)
 
 class Blog(Model):
-    __table__='blogs'
+    __table__ = 'blogs'
 
-    id=StringField(primary_key='True',default=next_id,ddl='varchar(50)')
-    user_id=StringField(ddl='varchar(50)')
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
     user_name = StringField(ddl='varchar(50)')
-    user_image=StringField(ddl='varchar(500)')
-    name=StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    name = StringField(ddl='varchar(50)')
     summary = StringField(ddl='varchar(200)')
     content = TextField()
     created_at = FloatField(default=time.time)
@@ -40,5 +47,3 @@ class Comment(Model):
     user_image = StringField(ddl='varchar(500)')
     content = TextField()
     created_at = FloatField(default=time.time)
-
-
